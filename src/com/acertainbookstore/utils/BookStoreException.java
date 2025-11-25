696a6ee17c9b0dd7d336883b6e036cf659ff471d
Christian Jordan <gcp547@alumni.ku.dk>
2025-11-25 18:23:40 +0100

    formatting

--- src/com/acertainbookstore/utils/BookStoreException.java
+++ src/com/acertainbookstore/utils/BookStoreException.java
@@ -5,45 +5,43 @@ package com.acertainbookstore.utils;
  */
 public class BookStoreException extends Exception {
 
-	/** The Constant serialVersionUID. */
-	private static final long serialVersionUID = 1L;
+    /**
+     * The Constant serialVersionUID.
+     */
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Instantiates a new {@link BookStoreException}.
-	 */
-	public BookStoreException() {
-		super();
-	}
+    /**
+     * Instantiates a new {@link BookStoreException}.
+     */
+    public BookStoreException() {
+        super();
+    }
 
-	/**
-	 * Instantiates a new {@link BookStoreException}.
-	 *
-	 * @param message
-	 *            the message
-	 */
-	public BookStoreException(String message) {
-		super(message);
-	}
+    /**
+     * Instantiates a new {@link BookStoreException}.
+     *
+     * @param message the message
+     */
+    public BookStoreException(String message) {
+        super(message);
+    }
 
-	/**
-	 * Instantiates a new {@link BookStoreException}.
-	 *
-	 * @param message
-	 *            the message
-	 * @param cause
-	 *            the cause
-	 */
-	public BookStoreException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    /**
+     * Instantiates a new {@link BookStoreException}.
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
+    public BookStoreException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-	/**
-	 * Instantiates a new {@link BookStoreException}.
-	 *
-	 * @param ex
-	 *            the exception
-	 */
-	public BookStoreException(Throwable ex) {
-		super(ex);
-	}
+    /**
+     * Instantiates a new {@link BookStoreException}.
+     *
+     * @param ex the exception
+     */
+    public BookStoreException(Throwable ex) {
+        super(ex);
+    }
 }
--- src/com/acertainbookstore/utils/BookStoreMessageTag.java
+++ src/com/acertainbookstore/utils/BookStoreMessageTag.java
@@ -6,33 +6,53 @@ package com.acertainbookstore.utils;
  */
 public enum BookStoreMessageTag {
 
-	/** The tag for the add book message. */
-	ADDBOOKS,
+    /**
+     * The tag for the add book message.
+     */
+    ADDBOOKS,
 
-	/** The tag for the list books message. */
-	LISTBOOKS,
+    /**
+     * The tag for the list books message.
+     */
+    LISTBOOKS,
 
-	/** The tag for the add copies message. */
-	ADDCOPIES,
+    /**
+     * The tag for the add copies message.
+     */
+    ADDCOPIES,
 
-	/** The tag for the get books message. */
-	GETBOOKS,
+    /**
+     * The tag for the get books message.
+     */
+    GETBOOKS,
 
-	/** The tag for the buy books message. */
-	BUYBOOKS,
+    /**
+     * The tag for the buy books message.
+     */
+    BUYBOOKS,
 
-	/** The tag for the update editor picks message. */
-	UPDATEEDITORPICKS,
+    /**
+     * The tag for the update editor picks message.
+     */
+    UPDATEEDITORPICKS,
 
-	/** The tag for the editor picks message. */
-	GETEDITORPICKS,
+    /**
+     * The tag for the editor picks message.
+     */
+    GETEDITORPICKS,
 
-	/** The tag for the remove all books message. */
-	REMOVEALLBOOKS,
+    /**
+     * The tag for the remove all books message.
+     */
+    REMOVEALLBOOKS,
 
-	/** The tag for the remove books message. */
-	REMOVEBOOKS,
+    /**
+     * The tag for the remove books message.
+     */
+    REMOVEBOOKS,
 
-	/** The tag for the get stock books by ISBN message. */
-	GETSTOCKBOOKSBYISBN;
+    /**
+     * The tag for the get stock books by ISBN message.
+     */
+    GETSTOCKBOOKSBYISBN,
 }
